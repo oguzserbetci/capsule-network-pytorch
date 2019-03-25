3d4a7d013389b9bf74d3394623da8684bc159fe2
Oguz Serbetci <oguz.serbetci@gmail.com>
2019-03-25 15:49:24 +0100

    Training script.

--- train.py
+++ train.py
@@ -5,18 +5,22 @@ Usage:
   train.py -r 3 -d 0.0005 -e 1 -b 128
 """
 
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 import torch
-from torch import nn
-from torch import functional as F
+from torch.nn import functional as F
 
-from model import CapsuleNetwork, Decoder
+from torchvision import datasets, transforms
+import torchvision.utils as vutils
 
-def train(load_path=None, save_path='', n_routing=3, decoder_loss=0, n_epochs=1, batch_size=128, log_interval=10, **kwargs):
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+
+from modules import CapsuleNetwork, Decoder
+
+
+def train(load_path=None, save_path='capsnet', n_routing=3, decoder_loss=0, n_epochs=1, batch_size=128, log_interval=10, test_interval=100, plot_reconstruction=False, writer=None, **kwargs):
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(
-            'data',
+            './data',
             train=True,
             download=True,
             transform=transforms.Compose([
@@ -28,49 +32,76 @@ def train(load_path=None, save_path='', n_routing=3, decoder_loss=0, n_epochs=1,
 
     caps_net = CapsuleNetwork(n_routing=n_routing)
     if load_path is not None:
-        caps_net.load_state_dict(torch.load(load_path))
+        caps_net.load_state_dict(torch.load(load_path + f'_r{n_routing}_d{decoder_loss}'))
+
+    params = caps_net.parameters()
     if decoder_loss > 0:
         decoder = Decoder()
+        params = list(params) + list(decoder.parameters())
+    optimizer = torch.optim.Adam(params)
 
-    optimizer = torch.optim.Adam(caps_net.parameters())
     caps_net.train()
-    losses = []
-    for epoch in range(n_epochs):
-        losses.append([])
-        for batch_idx, (data, target) in enumerate(train_loader):
+    for epoch in tqdm(range(n_epochs), unit='epoch'):
+        pbar = tqdm(enumerate(train_loader), unit='batch')
+        for batch_idx, (data, target) in pbar:
+            n_iter = epoch * len(train_loader) + batch_idx
+
             optimizer.zero_grad()
-            output = caps_net(data)
+            outputs, vectors = caps_net(data)
 
             onehot = onehot_(target, n_categories=10)
-            loss = margin_loss(output, onehot)
+            capsule_loss = margin_loss(outputs, onehot)
+            loss = capsule_loss
+            losses = dict(capsule=capsule_loss)
             if (decoder_loss > 0):
-                decoder_input = output * onehot.unsqueeze(-1)
+                decoder_input = vectors * onehot.unsqueeze(-1)
                 decoder_input = decoder_input.flatten(1)
                 reconstruction = decoder(decoder_input)
-                reconstruction_loss = torch.pairwise_distance(reconstruction, data.flatten(1), p=2).sum()
-                loss = loss + decoder_loss * reconstruction_loss
+                reconstruction_loss = decoder_loss * F.mse_loss(reconstruction, data.flatten(1), reduction='sum')
+                loss = capsule_loss + reconstruction_loss
+                losses.update(reconstruction=reconstruction_loss)
+
             loss.backward()
+            losses.update(joint=loss)
+            writer.add_scalars('training/losses', losses, n_iter)
+
             optimizer.step()
-            losses[-1].append(loss.item())
+
             if (batch_idx % log_interval == 0):
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                if plot_reconstruction:
+                    import matplotlib.pyplot as plt
+                    _, axes = plt.subplots(nrows=1, ncols=2,
+                                           sharex=True, sharey=True,
+                                           subplot_kw={'xticks': [],
+                                                       'yticks': []})
+                    axes[0].pcolor(data.flatten(1)[0].detach().numpy().reshape((28, 28)), cmap='Greys')
+                    axes[1].pcolor(reconstruction[0].detach().numpy().reshape((28, 28)), cmap='Greys')
+                    plt.savefig(save_path + f'{epoch}_{batch_idx}')
+                    writer.add_image('digit', vutils.make_grid(data[0], normalize=True, scale_each=True), n_iter)
+                    writer.add_image('reconstruction', vutils.make_grid(reconstruction[0].view(28, 28), normalize=True, scale_each=True), n_iter)
+
+                pbar.set_description('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} = {:.6f} + {:.6f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss.item()))
+                    100. * batch_idx / len(train_loader), loss.item(),
+                    capsule_loss, reconstruction_loss if decoder_loss > 0 else 0))
+
+            if ((batch_idx+1) % test_interval == 0):
+                test(caps_net, n_iter, batch_size, writer=writer, **kwargs)
 
     torch.save(caps_net.state_dict(), save_path + f'_r{n_routing}_d{decoder_loss}')
 
     return caps_net
 
 
-def test():
+def test(model, n_iter=0, batch_size=128, writer=None, **kwargs):
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(
-            'data',
-            train=False,
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307, ), (0.3081, ))
-            ])),
+        datasets.MNIST('./data',
+                       train=False,
+                       download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307, ), (0.3081, ))
+                       ])),
         batch_size=batch_size,
         shuffle=True)
 
@@ -79,20 +110,28 @@ def test():
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-            output = model(data)
-            test_loss += margin_loss(output,
-                                     target).item()  # sum up batch loss
-            pred = output.norm(
-                p=1, dim=-1).argmax(
-                    dim=-1,
-                    keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            output, _ = model(data)
+            onehot = onehot_(target, n_categories=10)
+            test_loss += margin_loss(output, onehot, reduce='sum').item()
+            pred = output.argmax(dim=-1, keepdim=True)  # get the index of the max log-probability
+            correct += (pred == target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Avg loss: {:.4f}, Acc: {}/{} Error: {:.3f}\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100 - (100. * correct / len(test_loader.dataset))))
+    error_rate = 100 - (100. * correct / len(test_loader.dataset))
+
+    print('\nTest set: Avg loss: {:.4f}, Ac: {}/{} Error: {:.3f}\n'.format(
+          test_loss, correct, len(test_loader.dataset),
+          error_rate))
+
+    writer.add_scalars('test/metrics', dict(error=error_rate, accuracy=correct/len(test_loader.dataset)), n_iter)
+
+
+def evaluate(**kwargs):
+    writer = SummaryWriter()
+    caps_net = train(writer=writer, **kwargs)
+    test(caps_net, writer=writer, **kwargs)
+    writer.close()
 
 
 def onehot_(categorical_labels, n_categories):
@@ -102,23 +141,17 @@ def onehot_(categorical_labels, n_categories):
     return onehot
 
 
-def margin_loss(vectors, target, reduce='mean'):
+def margin_loss(probs, target, reduce='mean'):
     '''
     vectors: vectors whose scales represent the log prob.
     target: onehot categorical labels.
     '''
-    ZERO = torch.Tensor([0.])
-
     M_P = 0.9
     M_M = 0.1
     LAMBDA = 0.5
 
-    prob = F.norm(vectors, p=1, dim=-1)
-
-    L = target * torch.max(ZERO, M_P - prob)**2
-    L += LAMBDA * (1. - target) * torch.max(
-        ZERO,
-        prob - M_M)**2
+    L = target * F.relu(M_P - probs)**2
+    L += LAMBDA * (1. - target) * F.relu(probs - M_M)**2
     loss = L.sum(-1)
 
     if reduce == 'mean':
@@ -126,25 +159,21 @@ def margin_loss(vectors, target, reduce='mean'):
     elif reduce == 'sum':
         return loss.sum()
     else:
-        raise ArgumentError
-
-
-def evaluate(**kwargs):
-    caps_net = train(**kwargs)
-    test(model)
+        raise ValueError()
 
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description="Train and Evaluate capsule network on MNIST.")
-    parser.add_argument('--save_path', '-s', help="Path to save the model after training.", default='')
+    parser.add_argument('--save_path', '-s', help="Path to save the model after training.", default='capsnet')
     parser.add_argument('--load_path', '-m', help="Path to saved model to be trained/evaluated.", default=None)
     parser.add_argument('--n_routing', '-r', type=int, help="Number of routing iterations for routing-by-agreement mechanism.", default=3)
     parser.add_argument('--decoder_loss', '-d', type=float, help="Scale of decoder loss.", default=0)
     parser.add_argument('--n_epochs', '-e', type=int, help="Number of epochs.", default=3)
     parser.add_argument('--batch_size', '-b', type=int, help="Batch size for training and testing.", default=128)
     parser.add_argument('--log_interval', '-l', type=int, help="Number of batches to report the training accuracy and loss.", default=10)
+    parser.add_argument('--plot_reconstruction', help="Flag to plot the reconstructions.", action='store_true')
 
     args = parser.parse_args()
     evaluate(**vars(args))
